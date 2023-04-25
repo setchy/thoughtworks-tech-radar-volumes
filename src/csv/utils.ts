ddac26584bd3adba8d504838a0b999fe2f285f93
Adam Setch <adam.setch@outlook.com>
2023-04-25 10:03:49 -0400

    fix: replace line returns with break tags

--- src/csv/utils.ts
+++ src/csv/utils.ts
@@ -1,3 +1,7 @@
 export function escapeDescriptionHTML(description: string): string {
-    return `"${description.replace(/"/g, '""')}"`;
+    const escapedDescription = description
+        .replace(/"/g, '""')
+        .replace(/\n/g, '<br>');
+
+    return `"${escapedDescription}"`;
 }
