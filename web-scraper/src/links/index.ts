20bc6f595bb61d369d8747159d0c587222713459
Adam Setch <adam.setch@outlook.com>
2023-08-03 06:56:38 +1000

    refactor: extract to module functions

--- web-scraper/src/links/index.ts
+++ web-scraper/src/links/index.ts
@@ -1,58 +1,37 @@
 import fs from 'fs';
 import { FILES, URLS } from '../common/constants';
-import puppeteer from 'puppeteer';
+import puppeteer, { Page } from 'puppeteer';
 import _ from 'lodash';
 
+const SELECTOR_RESULT_ITEM = 'a[data-testid="result-item__title"]';
+const links: string[] = [];
+
+let page: Page;
+
 export async function extractRadarLinks(): Promise<string[]> {
-    const links: string[] = [];
-
-    const PAGE_SIZE = 10;
-
     let i = 0;
     let totalRecords;
 
     const browser = await puppeteer.launch({ headless: 'new' });
+    page = await browser.newPage();
 
     while (true) {
-        const page = await browser.newPage();
+        await openResultsPageAndWaitForLoad(i);
 
-        // Navigate the page to a URL
-        await page.goto(`${URLS.SEARCH}?firstResult=${i}`);
-
-        // Wait for the elements to be present before extracting them
-        await page.waitForSelector('a[data-testid="result-item__title"]');
-
+        // On the first parse, extract and store the total records we're targeting to parse
         if (totalRecords === undefined) {
-            totalRecords = await page.evaluate(() => {
-                const spanElement = document.querySelector(
-                    'span[data-testid="pagination-info-text"]',
-                );
-                const text = spanElement?.textContent;
-                const regex = /(out of )(\d+)/;
-                const match = text?.match(regex);
-                return match ? parseInt(match[2]) : null;
-            });
+            totalRecords = await getTotalRecordCount();
         }
 
-        if (!totalRecords || i > totalRecords) {
+        if (!totalRecords || i === totalRecords) {
             break;
         }
 
-        // Extract the elements with the given data-testid attribute
-        const elements = await page.$$eval(
-            'a[data-testid="result-item__title"]',
-            (links) => links.map((link) => link.href),
-        );
+        const paginatedBlips = await getBlipResultsOnPage();
 
-        console.log(
-            `Adding items ${i + 1} through ${
-                i + elements.length
-            } to the list...`,
-        );
-        links.push(...elements);
-        links.sort();
+        addPagedResultsToLinks(paginatedBlips);
 
-        i += PAGE_SIZE;
+        i += paginatedBlips.length;
 
         // Add an artificial delay to reduce chance of CloudFront rate limiting
         await new Promise((r) => setTimeout(r, 5000));
@@ -61,10 +40,45 @@ export async function extractRadarLinks(): Promise<string[]> {
     browser.close();
 
     const uniqueLinks = _.uniq(links);
-
     fs.writeFileSync(FILES.DATA.LINKS, JSON.stringify(uniqueLinks, null, 4));
 
-    return links;
+    return uniqueLinks;
 }
 
 extractRadarLinks();
+
+async function getTotalRecordCount(): Promise<number | null> {
+    return await page.evaluate(() => {
+        const spanElement = document.querySelector(
+            'span[data-testid="pagination-info-text"]',
+        );
+        const text = spanElement?.textContent;
+        const regex = /(\d+)\sblips/;
+        const match = text?.match(regex);
+        return match ? parseInt(match[1]) : null;
+    });
+}
+
+async function getBlipResultsOnPage(): Promise<string[]> {
+    return await page.$$eval(SELECTOR_RESULT_ITEM, (links) =>
+        links.map((link) => link.href),
+    );
+}
+
+async function openResultsPageAndWaitForLoad(startingPosition: number) {
+    await page.goto(`${URLS.SEARCH}?firstResult=${startingPosition}`);
+
+    // Wait for the elements to be present before extracting them
+    await page.waitForSelector(SELECTOR_RESULT_ITEM);
+}
+
+function addPagedResultsToLinks(newLinks: string[]) {
+    console.log(
+        `Adding items ${links.length + 1} through ${
+            links.length + newLinks.length
+        } to the list...`,
+    );
+
+    links.push(...newLinks);
+    links.sort();
+}
