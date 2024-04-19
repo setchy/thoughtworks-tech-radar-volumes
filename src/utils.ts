cda9ad7253d2be9b5849670f2028900ab29724ce
Adam Setch <adam.setch@outlook.com>
2024-04-19 11:12:27 -0400

    fix: default encoding and maintain EOL

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,21 @@
 import fs from 'node:fs';
+import { EOL } from 'node:os';
+
+const DEFAULT_ENCODING = 'utf-8';
 
 export function readJSONFile<T>(filename: string): T {
-  return JSON.parse(fs.readFileSync(filename, 'utf8').toString());
+  return JSON.parse(fs.readFileSync(filename, DEFAULT_ENCODING).toString());
 }
 
 export function writeJSONFile(filename: string, contents: unknown) {
-  fs.writeFileSync(filename, `${JSON.stringify(contents, null, 2)}\n`);
+  fs.writeFileSync(
+    filename,
+    `${JSON.stringify(contents, null, 2)}\n`,
+    DEFAULT_ENCODING,
+  );
 }
 
 export function writeCSVFile(filename: string, data: string[]) {
-  fs.writeFileSync(filename, data.join('\n'));
+  fs.writeFileSync(filename, data.join('\n'), DEFAULT_ENCODING);
+  fs.appendFileSync(filename, EOL, DEFAULT_ENCODING);
 }
